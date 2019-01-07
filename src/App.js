ac1fdf255f28d9040b890c3716ebbbae865c3c8a
Niscolinx <35029903+Niscolinx@users.noreply.github.com>
2019-01-07 10:55:22 +0100

    Just starting out

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <header>
-       <table>
-         <tbody>
-           <td><img src="" alt=""/></td>
-           <td> <h1>The movie search</h1></td>
-         </tbody>
-       </table>
-      </header>
+      <div className='header'>
+       <h1>The movie search</h1>
+       <input type='text' className='header__input' placeholder='please search your movies here' />
       </div>
     );
   }
